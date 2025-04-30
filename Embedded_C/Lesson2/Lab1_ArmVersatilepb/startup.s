ac48e02d4c6be6ed776aaeb764e3ed6d51e29be3
MohamedElalawy <mohamedelalawey7@gmail.com>
2025-04-30 23:59:28 +0300

    Lesson2

--- Embedded_C/Lesson2/Lab1_ArmVersatilepb/startup.s
+++ Embedded_C/Lesson2/Lab1_ArmVersatilepb/startup.s
@@ -2,7 +2,7 @@
 .global reset            /* Make 'reset' visible to the linker*/
 
 reset:
-    ldr sp, =0x00011000  /* Set stack pointer*/
+    ldr sp, =stack_top  /* Set stack pointer*/
     bl main              /* Call main()*/
 
 stop: b stop             /* Infinite loop after main*/
