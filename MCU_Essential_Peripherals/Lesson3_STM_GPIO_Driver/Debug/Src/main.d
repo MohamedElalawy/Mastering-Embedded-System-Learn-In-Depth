a7e9dc128fdaa6c4eef70448442a19e9d54fae81
MohamedElalawy <mohamedelalawey7@gmail.com>
2025-06-18 10:51:52 +0300

    __

---
+++ MCU_Essential_Peripherals/Lesson3_STM_GPIO_Driver/Debug/Src/main.d
@@ -0,0 +1,7 @@
+Src/main.o: ../Src/main.c \
+ C:/Users/mm/STM32CubeIDE/Embedded/Drivers/stm32f103c6Drivers/inc/stm32f103c6_GPIO_Driver.h \
+ C:/Users/mm/STM32CubeIDE/Embedded/Drivers/stm32f103c6Drivers/inc/stm32f103x6.h \
+ C:/Users/mm/STM32CubeIDE/Embedded/Drivers/stm32f103c6Drivers/inc/stm32f103x6.h
+C:/Users/mm/STM32CubeIDE/Embedded/Drivers/stm32f103c6Drivers/inc/stm32f103c6_GPIO_Driver.h:
+C:/Users/mm/STM32CubeIDE/Embedded/Drivers/stm32f103c6Drivers/inc/stm32f103x6.h:
+C:/Users/mm/STM32CubeIDE/Embedded/Drivers/stm32f103c6Drivers/inc/stm32f103x6.h:
---
+++ MCU_Essential_Peripherals/Lesson3_STM_GPIO_Driver/Debug/stm32f103c6Drivers/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (10.3-2021.10)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../stm32f103c6Drivers/stm32f103c6_GPIO_Driver.c 
+
+OBJS += \
+./stm32f103c6Drivers/stm32f103c6_GPIO_Driver.o 
+
+C_DEPS += \
+./stm32f103c6Drivers/stm32f103c6_GPIO_Driver.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+stm32f103c6Drivers/%.o stm32f103c6Drivers/%.su stm32f103c6Drivers/%.cyclo: ../stm32f103c6Drivers/%.c stm32f103c6Drivers/subdir.mk
+	arm-none-eabi-gcc -gdwarf-2 "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DDEBUG -DSTM32 -DSTM32F1 -DSTM32F103C6Tx -c -I../Inc -I"C:/Users/mm/STM32CubeIDE/Embedded/Drivers/stm32f103c6Drivers/inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
+clean: clean-stm32f103c6Drivers
+
+clean-stm32f103c6Drivers:
+	-$(RM) ./stm32f103c6Drivers/stm32f103c6_GPIO_Driver.cyclo ./stm32f103c6Drivers/stm32f103c6_GPIO_Driver.d ./stm32f103c6Drivers/stm32f103c6_GPIO_Driver.o ./stm32f103c6Drivers/stm32f103c6_GPIO_Driver.su
+
+.PHONY: clean-stm32f103c6Drivers
+
